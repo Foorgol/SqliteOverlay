f7eb35be3eeb473b55c7e6e62fd1d39e90cf5b0e
Volker Knollmann <v.knollmann@web.de>
2016-10-30 16:33:24 +0100

    Added a .gitignore file and prepared build scripts for a new release.

---
+++ dist/build_0.3.1_Debian.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PKGVER=0.3.1
+PKGRELEASE=1
+PKGNAME=SqliteOverlay
+BUILDDIR=/tmp/$PKGNAME-build
+
+STARTDIR=$(pwd)
+
+mkdir $BUILDDIR
+pushd $BUILDDIR
+
+git clone -b $PKGVER --single-branch --depth 1 https://github.com/Foorgol/SqliteOverlay.git
+cd SqliteOverlay
+mkdir release
+cd release
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make
+echo "C++ lib for easier interfacing with SQLite databasses" > description-pak
+checkinstall --pkgversion $PKGVER \
+	--pkgrelease $PKGRELEASE \
+	--pkgname $PKGNAME \
+	--provides $PKGNAME \
+	--install=no \
+	--nodoc \
+	-y
+
+mv *deb $STARTDIR
+
+popd
+
+rm -rf $BUILDDIR
+
